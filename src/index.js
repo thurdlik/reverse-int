bfcf72e4e391eb90e7ab23d92d97d89f071944e0
Linar <>
2021-03-13 22:06:34 +0300

    feat: complete index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+        str = String(n);
+        ln = str.length;
+        myArr = [];
+        for (let i = ln - 1; i >= 0; i--) {
+            myArr.push(str[i]);
+        }
+        result = myArr.join([]);
+        return parseInt(result);
 }
